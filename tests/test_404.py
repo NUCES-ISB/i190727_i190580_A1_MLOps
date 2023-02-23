eaecfd7fefbc1dd42bd04f77051244b6652c0901
Talal Ahmed <talalahmed252@gmail.com>
2023-02-23 23:49:47 +0500

    test commit with co-author
    Co-authored-by: Ahmed <ahmedbilal2015@gmail.com>

--- tests/test_404.py
+++ tests/test_404.py
@@ -3,9 +3,10 @@ import json
 
 def test_404(app, client):
     print("IDHR AA GYA")
-    res = client.get('/')
+    res = client.get('/404')
     print(f"MY STATUS CODE: {str(res.status_code)}")
     assert res.status_code == 200
     expected = {'errorCode' : 404, 'message' : 'Route not found'}
-    print(json.loads(res.get_data(as_text=True)))
+    print(1, res.get_data(as_text=True))
+    print(2, json.loads(res.get_data(as_text=True)))
     assert expected == json.loads(res.get_data(as_text=True))
