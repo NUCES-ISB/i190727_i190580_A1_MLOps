3c3a119779286b2a42b38838854ee52806343cfc
Talal Ahmed <talalahmed252@gmail.com>
2023-02-23 23:29:25 +0500

    updated tests file

--- tests/coftest.py
+++ tests/coftest.py
@@ -7,9 +7,3 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
-
-def test_404(app, client):
-    res = client.get('/')
-    assert res.status_code == 200
-    expected = {'errorCode' : 404, 'message' : 'Route not found'}
-    assert expected == json.loads(res.get_data(as_text=True))
---
+++ tests/test_404.py
@@ -0,0 +1,8 @@
+import json
+
+
+def test_404(app, client):
+    res = client.get('/')
+    assert res.status_code == 200
+    expected = {'errorCode' : 404, 'message' : 'Route not found'}
+    assert expected == json.loads(res.get_data(as_text=True))
