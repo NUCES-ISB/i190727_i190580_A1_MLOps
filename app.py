dddc968e21834a2ffd518165ecacf08e361b22e2
Ahmed Bilal <ahmedbilal2015@gmail.com>
2023-02-24 00:00:48 +0500

    added black formatter

--- app.py
+++ app.py
@@ -139,7 +139,6 @@ def settings():
 
 
 @app.route('/')
-
 # ======== Main ============================================================== #
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True, host="0.0.0.0")
